05510aabb5da8dbe9852f1b9681b1d98a217cc39
Brandon Cope <hunterv96@gmail.com>
2022-03-21 20:06:49 -0500

    finished

---
+++ src/app/labeled-input/labeled-input.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-labeled-input',
+  templateUrl: './labeled-input.component.html',
+  styleUrls: ['./labeled-input.component.css']
+})
+export class LabeledInputComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
